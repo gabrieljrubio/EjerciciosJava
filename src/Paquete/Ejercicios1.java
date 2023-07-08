92ffd5d366e134050b9e3d19f4853762a4b717d0
Gabriel Rubio <112970014+gabrieljrubio@users.noreply.github.com>
2023-07-08 11:19:06 +0200

    Initial commit

---
+++ src/Paquete/Ejercicios1.java
@@ -0,0 +1,20 @@
+package Paquete;
+
+public class Ejercicios1 {
+
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        long numero2 = 2;
+        double numero3 = 4.5d;
+        boolean verdadero = true;
+        String saludo = "Hola Mundo";
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(verdadero);
+        System.out.println(saludo);
+
+    }
+}
